2bfa1d7c51d508917929087c581d36bd91b31838
Tobias Petersen <tdp@exerp.com>
2023-01-17 10:33:31 +0100

    fix(): small changes on debug prints

--- refresh-cleanup/lambda_function.py
+++ refresh-cleanup/lambda_function.py
@@ -12,8 +12,7 @@ def deleteRDS(instance: dict, rds_client: boto3) -> bool:
             DBInstanceIdentifier=identifier,
             SkipFinalSnapshot=True,
             DeleteAutomatedBackups=True)["DBInstance"]
-        # Waiting 2 sec to make sure AWS corrects instance status
-        time.sleep(2)
+        time.sleep(2) # Waiting 2 sec to make sure AWS corrects instance status
         response = rds_client.describe_db_instances(
             DBInstanceIdentifier=identifier)["DBInstances"]
         if response["DBInstanceStatus"] == "deleting":
@@ -27,15 +26,13 @@ def deleteRDS(instance: dict, rds_client: boto3) -> bool:
 def checkInstance(instance: dict):
     instance_name = instance['DBInstanceIdentifier']
     if not instance["DBInstanceStatus"] == "stopped":
-        print(
-            f"instance {instance_name} is not in status: -stopped- but in status: -{instance['DBInstanceStatus']}-")
+        print(f"instance {instance_name} is not in status: -stopped- but in status: -{instance['DBInstanceStatus']}-")
         return False
     instance_restart_time: datetime.datetime = instance["AutomaticRestartTime"]
     irt_formatted = instance_restart_time.strftime("%d/%m/%Y")
     date_today = datetime.date.today().strftime("%d/%m/%Y")
     if not date_today == irt_formatted:
-        print(
-            f"{instance_name} deletion date is {irt_formatted} and not today {date_today}")
+        print(f"{instance_name} deletion date is {irt_formatted} and not today {date_today}")
         return False
     return True
 
@@ -56,5 +53,7 @@ def lambda_handler(event, context):
                 if deleteRDS(instance, rds_client):
                     instance_list.append("db_identifier")
     if instance_list:
-        return f"successfully deleted instance: {instance_list}"
+        print(f"Deleting instance: {instance_list}")
+        return f"Deleting instance: {instance_list}"
+    print(f"Nothing to delete")
     return f"Nothing to delete"
